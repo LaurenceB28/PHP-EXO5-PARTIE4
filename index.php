7a2727c7ac4686e9c663f60e174050f2c9f738c3
LaurenceB28 <laurence.brd@gmail.com>
2020-11-23 14:03:34 +0100

    exo5phpP4

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    for ($i=0; $i<= 300; $i++) {
+    // echo "$i:";
+    if ($i % 2 != 0)
+    {
+        echo  $i . ', ';
+    }
+}
+    ?>
+</body>
+</html>
